a0b9f89645bd5572043cdacc2881688cc50b568d
eric <eric@dev.lan>
2022-11-18 21:52:03 +0100

    add new model Chat and add reference with message

--- chat/models.py
+++ chat/models.py
@@ -1,13 +1,16 @@
 from django.db import models
-from django.db.models.fields import DateField
 from datetime import date
 from django.conf import settings
 from django.utils.timezone import now
 
 # Create your models here.
+class Chat(models.Model):
+    created_at = models.DateField(default=now)
+
 class Message(models.Model):
     text = models.CharField(max_length=500)
-    created_at = DateField(default=now)
-    #chat = mit chat vernüofen
+    created_at = models.DateField(default=now)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
+
