f007df85854ff98a69e6887fe5815b8e75055371
eric <eric@dev.lan>
2022-11-19 15:02:23 +0100

    show all created messages of chat 2 to the user

--- chat/views.py
+++ chat/views.py
@@ -6,6 +6,13 @@ def index(request):
     # print(request)
     if request.method == 'POST':
         print('reveided data ' + request.POST['message'])
-        chat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['message'], chat=chat, author=request.user, receiver=request.user)
-    return render(request, 'chat/index.html', {'username': 'Eric'})
+        myChat = Chat.objects.get(id=2)
+        Message.objects.create(
+            text=request.POST['message'], chat=myChat, author=request.user, receiver=request.user
+        )
+    chatMessages = Message.objects.filter(chat__id = 2)
+    return render(request, 'chat/index.html', {'messages': chatMessages})
+
+    #chats = Chat.objects.get()
+    # print(chats)
+    # print(chats.id)
