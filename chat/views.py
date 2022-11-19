63175124f0b44e75d8004e095c6438a8422108b4
eric <eric@dev.lan>
2022-11-19 13:35:46 +0100

    save message so that it is connected to chat

--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Message, Chat
 
 
 def index(request):
     # print(request)
     if request.method == 'POST':
         print('reveided data ' + request.POST['message'])
+        chat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['message'], chat=chat, author=request.user, receiver=request.user)
     return render(request, 'chat/index.html', {'username': 'Eric'})
