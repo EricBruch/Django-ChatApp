300728b8d8bbe101087e8c05260e4deddd1242a2
eric <eric@dev.lan>
2022-11-21 22:45:03 +0100

    create message in fe based on response from be

--- chat/views.py
+++ chat/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
 from .models import Message, Chat
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.core import serializers
+
 
 @login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
-        print('reveided data ' + request.POST['message'])
         myChat = Chat.objects.get(id=2)
-        Message.objects.create(
+        message = Message.objects.create(
             text=request.POST['message'], chat=myChat, author=request.user, receiver=request.user
         )
-    chatMessages = Message.objects.filter(chat__id = 2)
+        serialized_obj = serializers.serialize('json', [message])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
+    chatMessages = Message.objects.filter(chat__id=2)
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
     #chats = Chat.objects.get()
